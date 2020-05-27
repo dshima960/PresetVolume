b8067c7568b59a71a65814f4b73c4471c071b738
Daisuke Shima <shima@cba-japan.com>
2020-05-27 11:57:41 +0900

    プロジェクト ファイルを追加します。

---
+++ PresetVolume/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PresetVolume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VolumeController vc = new VolumeController();
+            vc.SetVolume(Convert.ToInt32(Properties.Settings.Default["preset1"]));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            VolumeController vc = new VolumeController();
+            vc.SetVolume(Convert.ToInt32(Properties.Settings.Default["preset2"]));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            VolumeController vc = new VolumeController();
+            vc.SetVolume(Convert.ToInt32(Properties.Settings.Default["preset3"]));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            VolumeController vc = new VolumeController();
+            vc.SetVolume(Convert.ToInt32(Properties.Settings.Default["preset4"]));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            VolumeController vc = new VolumeController();
+            vc.SetVolume(Convert.ToInt32(Properties.Settings.Default["preset5"]));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = (string)Properties.Settings.Default["preset1"];
+            button2.Text = (string)Properties.Settings.Default["preset2"];
+            button3.Text = (string)Properties.Settings.Default["preset3"];
+            button4.Text = (string)Properties.Settings.Default["preset4"];
+            button5.Text = (string)Properties.Settings.Default["preset5"];
+        }
+    }
+}
---
+++ PresetVolume/VolumeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+
+namespace PresetVolume
+{
+    class VolumeController
+    {
+        public void SetVolume(int value)
+        {
+            //音量を変更
+            MMDevice device;
+            MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+            device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = ((float)value / 100.0f);
+        }
+    }
+}
